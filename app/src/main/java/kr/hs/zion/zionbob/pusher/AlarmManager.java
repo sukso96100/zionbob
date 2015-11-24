35580e3b3e3a17304ae1b3c54cdc0c40f849779b
sukso96100 <sukso96100@gmail.com>
2015-11-24 23:09:56 +0900

    update

--- app/src/main/java/kr/hs/zion/zionbob/pusher/AlarmManager.java
+++ app/src/main/java/kr/hs/zion/zionbob/pusher/AlarmManager.java
@@ -4,4 +4,12 @@ package kr.hs.zion.zionbob.pusher;
  * Created by youngbin on 15. 11. 24.
  */
 public class AlarmManager {
+
+    void registerAlarm(){
+
+    }
+
+    void calcelAlarm(){
+
+    }
 }
