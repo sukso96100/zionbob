e5aff7f59b20ee6d5d198a14509440e36866d70e
sukso96100 <sukso96100@gmail.com>
2015-11-24 09:04:07 +0900

    working on meal data caching

--- app/src/main/java/kr/hs/zion/zionbob/MealDataCacheManager.java
+++ app/src/main/java/kr/hs/zion/zionbob/MealDataCacheManager.java
@@ -1,7 +1,60 @@
 package kr.hs.zion.zionbob;
 
+import android.content.Context;
+import android.util.Log;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
 /**
  * Created by youngbin on 15. 11. 24.
  */
 public class MealDataCacheManager {
+    String TAG = "MealDataCacheManager";
+
+    Context mContext;
+    Realm mRealm;
+
+    public MealDataCacheManager(Context c){
+        mContext = c;
+        mRealm = Realm.getInstance(mContext);
+    }
+
+    public void updateCache(String Date, String Meal,
+                            String Origin, String Nutrients){
+        RealmQuery<MealDataCacheModel> query = mRealm.where(MealDataCacheModel.class);
+        query.equalTo("date",Date);
+        MealDataCacheModel result = query.findFirst();
+        if(result==null){
+            Log.d(TAG, "Caching New Data");
+            mRealm.beginTransaction();
+            MealDataCacheModel MDCM = mRealm.createObject(MealDataCacheModel.class);
+            MDCM.setDate(Date);
+            MDCM.setMeal(Meal);
+            MDCM.setOrigin(Origin);
+            MDCM.setNutrients(Nutrients);
+            mRealm.commitTransaction();
+        }else{
+            Log.d(TAG, "Updating Cached Data");
+            mRealm.beginTransaction();
+            result.setMeal(Meal);
+            result.setOrigin(Origin);
+            result.setNutrients(Nutrients);
+            mRealm.commitTransaction();
+        }
+    }
+
+    public String[] getFromCache(String Date){
+        RealmQuery<MealDataCacheModel> query = mRealm.where(MealDataCacheModel.class);
+        query.equalTo("date",Date);
+        MealDataCacheModel result = query.findFirst();
+        String CachedData[] = new String[4];
+        CachedData[0] = result.getDate();
+        CachedData[1] = result.getMeal();
+        CachedData[2] = result.getOrigin();
+        CachedData[3] = result.getNutrients();
+        return CachedData;
+    }
 }
---
+++ app/src/main/java/kr/hs/zion/zionbob/MealDataCacheModel.java
@@ -0,0 +1,49 @@
+package kr.hs.zion.zionbob;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by youngbin on 15. 11. 24.
+ */
+public class MealDataCacheModel extends RealmObject {
+    private String Date; // Formatting Example : 2015.11.24_2
+    private String Meal;
+    private String Origin;
+    private String Nutrients;
+
+    // Getters
+
+    public String getDate() {
+        return Date;
+    }
+
+    public String getMeal() {
+        return Meal;
+    }
+
+    public String getOrigin() {
+        return Origin;
+    }
+
+    public String getNutrients() {
+        return Nutrients;
+    }
+
+    // Setters
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public void setMeal(String meal) {
+        Meal = meal;
+    }
+
+    public void setOrigin(String origin) {
+        Origin = origin;
+    }
+
+    public void setNutrients(String nutrients) {
+        Nutrients = nutrients;
+    }
+}
