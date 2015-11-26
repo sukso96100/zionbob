53ea4c165ed3f41e9548faa8425a5d65872d7499
sukso96100 <sukso96100@gmail.com>
2015-11-26 12:27:11 +0900

    working on meal push notification

---
+++ app/src/main/java/kr/hs/zion/zionbob/data/MealDataLoader.java
@@ -0,0 +1,7 @@
+package kr.hs.zion.zionbob.data;
+
+/**
+ * Created by youngbin on 15. 11. 26.
+ */
+public class MealDataLoader {
+}
--- app/src/main/java/kr/hs/zion/zionbob/pusher/MealPushService.java
+++ app/src/main/java/kr/hs/zion/zionbob/pusher/MealPushService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.Calendar;
+
 public class MealPushService extends Service {
     public MealPushService() {
     }
@@ -12,5 +14,25 @@ public class MealPushService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        // Get Date of Today
+        Calendar Cal = Calendar.getInstance();
+        int year = Cal.get(Calendar.YEAR);
+        int month = Cal.get(Calendar.MONTH);
+        int day = Cal.get(Calendar.DAY_OF_MONTH);
+        int dayofweek = Cal.get(Calendar.DAY_OF_WEEK);
+
+        // Get Meal Type
+        int mealtype = intent.getIntExtra("mealtype",2);
+
+        // Create Date String
+        String DATE = year+"."+month+"."+day;
+
+
+        return START_NOT_STICKY;
     }
 }
